901581836be59630c479999cf95062b58023022b
xiazhiqiang <xiazhiqiang@timeface.cn>
2016-07-01 18:27:01 +0800

    angularjs

---
+++ angularjs/7过滤器.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ProCloud on 2016/7/1.
+ */
+var app=angular.module('myApp',[]);
+app.controller('myCtrl',function ($scope) {
+    $scope.name = "xiazhiqiang";
+    $scope.money = 500;
+});
